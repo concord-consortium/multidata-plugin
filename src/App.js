9d1d0461e0fa935acbdd0314bc5907bf344f5fa3
lublagg <lvblagg@gmail.com>
2023-01-17 17:10:27 -0500

    Add padding option.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React, {useEffect} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { useCodapState } from './hooks/useCodapState';
-import { AgGridReact } from 'ag-grid-react';
-
-import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-alpine.css';
 
 function App() {
   const { dataSets, selectedDataSet, collections, items, handleSelectDataSet } = useCodapState();
+  const [padding, setPadding] = useState(false);
+
+  const mapHeadersFromValues = (values) => {
+    return (
+      <>
+        {(Object.keys(values)).map((key) => {
+          if (typeof values[key] === "string" || typeof values[key] === "number") {
+              return (<th>{key}</th>);
+            }
+          }
+        )}
+      </>
+    );
+  }
+
+  const mapCellsFromValues = (values) => {
+    return (
+      <>
+        {Object.values(values).map(val => {
+          if (typeof val === "string" || typeof val === "number") {
+              return (<td>{val}</td>);
+            }
+          }
+        )}
+      </>
+    );
+  }
 
   const renderSingleTable = () => {
     const collection = collections[0];
@@ -19,20 +42,12 @@ function App() {
               <th>
                 {attr.title}
               </th>
-            )
+            );
           })}
         </tr>
         {items.length && items.map((item) => {
           return (
-            <tr>{Object.values(item).map(val => {
-              if (typeof val === "string" || typeof val === "number") {
-                  return (
-                  <td>
-                    {val}
-                  </td>
-                  );
-                }
-              })}</tr>
+            <tr>{mapCellsFromValues(item)}</tr>
           )
         })}
       </>
@@ -44,72 +59,38 @@ function App() {
   }
 
   const renderRowFromCaseObj = (caseObj) => {
-    if (!caseObj.children.length) {
+    const {children, values} = caseObj;
+    if (!children.length) {
       return (
-        <tr>{(Object.values(caseObj.values)).map(val => {
-          if (typeof val === "string" || typeof val === "number") {
-              return (
-              <td>
-                {val}
-              </td>
-              );
-            }
-          })}</tr>
+        <tr>{mapCellsFromValues(values)}</tr>
       )
     } else {
       return (
         <>
         <tr>
-          {(Object.values(caseObj.values)).filter(val => typeof val === "string" || typeof val === "number").length > 1 ?
-            <td>
-              <table>
+          {(Object.values(values)).filter(val => typeof val === "string" || typeof val === "number").length > 1 ?
+            <td style={{padding: padding ? "7px" : "0px", border: padding ? "1px solid black" : "0px"}}>
+              <table className="sub-table">
                 <tbody>
-                  <tr>{(Object.keys(caseObj.values)).map((key) => {
-                    if (typeof caseObj.values[key] === "string" || typeof caseObj.values[key] === "number") {
-                        return (
-                        <th>
-                          {key}
-                        </th>
-                        );
-                      }
-                    })}
-                  </tr>
-                  <tr>
-                    {(Object.values(caseObj.values)).map((val) => {
-                      if (typeof val === "string" || typeof val === "number") {
-                          return (
-                          <td>
-                            {val}
-                          </td>
-                          );
-                        }
-                      })}
-                  </tr>
+                  <tr className="sub-header-row">{mapHeadersFromValues(values)}</tr>
+                  <tr>{mapCellsFromValues(values)}</tr>
                 </tbody>
               </table>
             </td>
-            :(Object.values(caseObj.values)).map((val) => {
-            if (typeof val === "string" || typeof val === "number") {
-                return (
-                <td>
-                  {val}
-                </td>
-                );
-              }
-            })
+            : mapCellsFromValues(values)
           }
-          <td>
-            <table className="sub-table">
+          <td className={"main-td"} style={{padding: padding ? "7px" : "0px"}}>
+            <table style={{padding: padding ? "7px" : "0px", border: padding ? "1px solid black" : "none"}} className="sub-table">
               <tbody>
                 {caseObj.children.map((child, i) => {
                   if (i === 0) {
                     return (
                       <>
-                      <tr className="sub-header-row">
-                        {((Object.keys(child.values)).map(key => <th>{key}</th>))}
-                        {child.children.length ? <th>{child.children[0].collection.name}</th> : ""}
-                      </tr>
-                      {renderRowFromCaseObj(child)}
+                        <tr className="sub-header-row">
+                          {mapHeadersFromValues(child.values)}
+                          {child.children.length ? <th>{child.children[0].collection.name}</th> : <></>}
+                        </tr>
+                        {renderRowFromCaseObj(child)}
                       </>
                       );
                   } else {
@@ -125,14 +106,13 @@ function App() {
     }}
 
   const renderTable = () => {
-    console.log("collections", collections);
     return (
       <table className="main-table">
-        <tbody>
+        <tbody className="main-tbody">
           <tr className="main-header-row">
             {
-              collections.length === 1 ? <th>{collections[0].title}</th> :
-              collections.filter((c, i) => i === 0 || i === 1).map((c) => <th>{c.title}</th>)
+              collections.length === 1 ? <th className="main-header">{collections[0].title}</th> :
+              collections.filter((c, i) => i === 0 || i === 1).map((c) => <th className="main-header">{c.title}</th>)
             }
           </tr>
           {
@@ -144,13 +124,28 @@ function App() {
     );
   }
 
+  const togglePadding = () => {
+    if (padding) {
+      setPadding(false)
+    } else {
+      setPadding(true);
+    }
+  }
+
   return (
     <div>
-      <div className="data-sets">
-        <select onChange={handleSelectDataSet}>
-          <option default></option>
-          {dataSets && dataSets.length && dataSets.map((set) => {return (<option>{set.title}</option>)})}
-        </select>
+      <div className="controls">
+        <div className="data-sets">
+          <span>Select a Dataset:</span>
+          <select onChange={handleSelectDataSet}>
+            <option default></option>
+            {dataSets && dataSets.length && dataSets.map((set) => {return (<option>{set.title}</option>)})}
+          </select>
+        </div>
+        <div className="set-padding">
+          <span>Padding?</span>
+          <input type="checkbox" onChange={togglePadding}/>
+        </div>
       </div>
       {selectedDataSet && collections.length && renderTable()}
   </div>);
--- src/scripts/notify.js
+++ src/scripts/notify.js
@@ -1,142 +1,83 @@
-import { connect } from "./connect";
+// import { connect } from "./connect";
 
-export const notify = {
-  setUpDocumentNotifications: async function () {
-      //  receive notifications about doc changes, especially number of datasets
-      //  (user has added or deleted a dataset)
-      const tResource = `documentChangeNotice`;
-      codapInterface.on(
-          'notify',
-          tResource,
-          //  'updateAttribute',
-          notify.handleDocumentChangeNotice
-      );
-  },
-  /**
-   * Ask to be notified about changes in attributes and selections
-   * @returns {Promise<void>}
-   */
-  setUpNotifications: async function () {
-    const name = connect.state.selectedDataSet;
-    if (name) {
-      //  register to receive notifications about changes to the data context (including selection)
-      const sResource = `dataContextChangeNotice[${name}]`;
-      codapInterface.on(
-          'notify',
-          sResource,
-          //'selectCases',
-          notify.handleDataContextChangeNotice
-      );
-    }
-  },
+// export const notify = {
+//   setUpDocumentNotifications: async function () {
+//       //  receive notifications about doc changes, especially number of datasets
+//       //  (user has added or deleted a dataset)
+//       const tResource = `documentChangeNotice`;
+//       codapInterface.on(
+//           'notify',
+//           tResource,
+//           //  'updateAttribute',
+//           notify.handleDocumentChangeNotice
+//       );
+//   },
+//   /**
+//    * Ask to be notified about changes in attributes and selections
+//    * @returns {Promise<void>}
+//    */
+//   setUpNotifications: async function () {
+//     const name = connect.state.selectedDataSet;
+//     if (name) {
+//       //  register to receive notifications about changes to the data context (including selection)
+//       const sResource = `dataContextChangeNotice[${name}]`;
+//       codapInterface.on(
+//           'notify',
+//           sResource,
+//           //'selectCases',
+//           notify.handleDataContextChangeNotice
+//       );
+//     }
+//   },
 
-  handleDataContextChangeNotice: function (iMessage) {
-      const name = connect.state.selectedDataSet;
-      if (iMessage.resource === `dataContextChangeNotice[${name}]`) {
-          this.nHandled++;
-          if (this.nHandled % 50 === 0) {
-          }
+//   handleDataContextChangeNotice: function (iMessage) {
+//       const name = connect.state.selectedDataSet;
+//       if (iMessage.resource === `dataContextChangeNotice[${name}]`) {
+//           this.nHandled++;
+//           if (this.nHandled % 50 === 0) {
+//           }
 
-          const theValues = iMessage.values;
-          switch (theValues.operation) {
-              case `selectCases`:
-              case `updateCases`:
-                  choosy.handlers.handleSelectionChangeFromCODAP();
-                  break;
+//           const theValues = iMessage.values;
+//           switch (theValues.operation) {
+//               case `selectCases`:
+//               case `updateCases`:
+//                   choosy.handlers.handleSelectionChangeFromCODAP();
+//                   break;
 
-              case `updateCollection`:
-              case `createCollection`:
-              case `deleteCollection`:
-              case `moveAttribute`:
-              case `deleteAttributes` :
-              case `createAttributes` :
-              case `updateAttributes`:
-              case `hideAttributes`:
-              case `showAttributes`:
+//               case `updateCollection`:
+//               case `createCollection`:
+//               case `deleteCollection`:
+//               case `moveAttribute`:
+//               case `deleteAttributes` :
+//               case `createAttributes` :
+//               case `updateAttributes`:
+//               case `hideAttributes`:
+//               case `showAttributes`:
 
-                  choosy_ui.update();     //  which reads the database structure (cols, atts) from CODAP
-                  break;
-              //  todo: alter when JS fixes the bug about not issuing notifications for plugin-initiated changes.
+//                   choosy_ui.update();     //  which reads the database structure (cols, atts) from CODAP
+//                   break;
+//               //  todo: alter when JS fixes the bug about not issuing notifications for plugin-initiated changes.
 
-              case `updateDataContext`:       //  includes renaming dataset, so we have to redo the menu
-                  choosy.setUpDatasets();
-                  choosy_ui.update();
-                  break;
+//               case `updateDataContext`:       //  includes renaming dataset, so we have to redo the menu
+//                   choosy.setUpDatasets();
+//                   choosy_ui.update();
+//                   break;
 
-              case 'createCases':
-              case 'createItems':
-                  break;
+//               case 'createCases':
+//               case 'createItems':
+//                   break;
 
-              default:
-                  break;
-          }
-      }
-  },
+//               default:
+//                   break;
+//           }
+//       }
+//   },
 
-  handleDocumentChangeNotice: function (iMessage) {
-      this.nHandled++;
-      // choosy.log(`handleDocumentChange operation: ${theValues.operation}`);
-      choosy.setUpDatasets();
-  },
+//   handleDocumentChangeNotice: function (iMessage) {
+//       this.nHandled++;
+//       // choosy.log(`handleDocumentChange operation: ${theValues.operation}`);
+//       choosy.setUpDatasets();
+//   },
 
-}
+// }
 
-
-  // const setUpDocumentNotifications = async () => {
-  //   console.log("I am setUpDocumentNotifications");
-  //   const tResource = `documentChangeNotice`;
-  //   codapInterface.on(
-  //       'notify',
-  //       tResource,
-  //       //  'updateAttribute',
-  //       handleDocumentChangeNotice
-  //   );
-  // };
-
-  // const setUpNotifications = async () => {
-  //   //  register to receive notifications about changes to the data context (including selection)
-  //   const sResource = `dataContextChangeNotice[${selectedDataSet}]`;
-  //   codapInterface.on(
-  //       'notify',
-  //       sResource,
-  //       //'selectCases',
-  //       handleDataContextChangeNotice
-  //   );
-  // };
-
-  // const handleStateUpdate = () => {
-  //   console.log("datasetupdates", dataSetUpdates);
-  //   setDataSetUpdates([...dataSetUpdates, 'true']);
-  // }
-
-  // const handleDocumentChangeNotice = (iMessage) =>{
-  //   console.log("handleDocumentChangeNotice IMessage", iMessage);
-  //   getDataSets();
-  // }
-
-  // const handleUpdate = () => {
-  //   console.log("I AM HANDLE UPDATE!");
-  //   console.log("dataSetUpdates before I change it", dataSetUpdates);
-  //   console.log("[...dataSetUpdates] before I change it", [...dataSetUpdates]);
-  //   const newUpdates = [...dataSetUpdates];
-  //   console.log("newUpdates before I change it", newUpdates);
-  //   newUpdates.push("update");
-  //   console.log("newUpdates after I change it", newUpdates);
-  //   setDataSetUpdates(newUpdates);
-  // }
-
-  // const handleDataContextChangeNotice = (iMessage) => {
-  //   console.log("handleDataContextChangeNotice IMessage", iMessage);
-  //   if (iMessage.resource === `dataContextChangeNotice[${selectedDataSet}]`) {
-  //     const theValues = iMessage.values;
-  //     console.log("theValues.operation", theValues.operation);
-  //     switch (theValues.operation) {
-  //       case 'createCollection':
-  //         handleUpdate();
-  //         break;
-  //       default:
-  //         handleUpdate();
-  //         break;
-  //     }
-  //   }
-  // };
