b10adfec6e1e6dfa6d0120e4bceadf9ffc7261b7
Ethan McElroy <emcelroy@concord.org>
2024-09-13 11:42:42 -0400

    refactor: adjust handlSubmit and add styling

--- src/components/editable-table-cell.tsx
+++ src/components/editable-table-cell.tsx
@@ -2,6 +2,8 @@ import React, { ReactNode, useState } from "react";
 import { Editable, EditablePreview, EditableInput } from "@chakra-ui/react";
 import { updateCaseById } from "@concord-consortium/codap-plugin-api";
 
+import css from "./editable-table-cell.scss";
+
 interface IProps {
   attrTitle: string;
   caseId: string;
@@ -21,31 +23,35 @@ export const EditableTableCell = (props: IProps) => {
   };
 
   const handleCancel = () => {
+    setEditingValue(displayValue);
     setIsEditing(false);
   };
 
-  const handleSubmit = async (nextValue: string) => {
+  const handleSubmit = async (newValue: string) => {
     try {
-      await updateCaseById(selectedDataSetName, caseId, {[attrTitle]: nextValue});
+      await updateCaseById(selectedDataSetName, caseId, {[attrTitle]: newValue});
+      setEditingValue(newValue);
+      setIsEditing(false);
+      handleUpdateCollections();
     } catch (e) {
       console.error("Case not updated: ", e);
     }
-    setIsEditing(false);
-    handleUpdateCollections();
   };
 
   return (
-    <Editable
-      isPreviewFocusable={true}
-      onCancel={handleCancel}
-      onChange={handleChangeValue}
-      onEdit={() => setIsEditing(true)}
-      onSubmit={handleSubmit}
-      submitOnBlur={true}
-      value={isEditing ? editingValue : displayValue}
-    >
-      {!isEditing && <EditablePreview />}
-      <EditableInput value={editingValue} />
-    </Editable>
+    <div className={css.editableTableCell}>
+      <Editable
+        isPreviewFocusable={true}
+        onCancel={handleCancel}
+        onChange={handleChangeValue}
+        onEdit={() => setIsEditing(true)}
+        onSubmit={handleSubmit}
+        submitOnBlur={true}
+        value={isEditing ? editingValue : displayValue}
+      >
+        {!isEditing && <EditablePreview />}
+        <EditableInput />
+      </Editable>
+    </div>
   );
 };
